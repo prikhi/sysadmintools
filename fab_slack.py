5559ec20ec97b614f5d10e84c054916e9f405aae
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2013-11-20 21:07:16 -0500

    Add Slackware Server Fabric File

---
+++ fab_slack.py
@@ -0,0 +1,46 @@
+"""
+This Fabric file contains commands for Administrating Acorn's Servers.
+
+It is assumed we are currently running Slackware Linux.
+
+Usage:
+
+    List available commands with ``fab -f fab_slack.py -l``
+    Run commands with ``fab -f fab_slack.py command:args,kwarg=value``
+
+"""
+from fabric.api import prompt, sudo, env
+
+
+env.hosts = ['vishnu.acorn', 'aphrodite.acorn', 'adonis.acorn']
+
+
+def update_package_list():
+    """Update the list of packages from the current repository."""
+    sudo("/usr/sbin/slackpkg update -batch=on")
+
+
+def upgrade_all_packages():
+    """Upgrade all packages to the latest version."""
+    sudo("/usr/sbin/slackpkg upgrade-all -batch=on")
+
+
+def upgrade_package(package_name=None):
+    """Upgrade the specified package."""
+    if package_name is None:
+        package_name = prompt("Which package?")
+    update_package_list()
+    sudo("/usr/sbin/slackpkg upgrade {}".format(package_name))
+
+
+def remove_package(package_name=None):
+    """Remove the specified package."""
+    if package_name is None:
+        package_name = prompt("Which package?")
+    sudo("/usr/sbin/slackpkg remove {}".format(package_name))
+
+
+def update_and_upgrade():
+    """Update the package list and Upgrade all packages."""
+    update_package_list()
+    upgrade_all_packages()
